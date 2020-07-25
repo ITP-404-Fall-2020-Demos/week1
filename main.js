ca4e0fb1de2e75085dcfef6e64fdd659da3298c5
David Tang <dtang@auditboard.com>
2020-07-24 22:16:33 -0700

    4. Fetch and render repos

--- main.js
+++ main.js
@@ -20,3 +20,31 @@ $("#members-link").on("click", function () {
     $("#results").html(fragment);
   });
 });
+
+$("#repos-link").on("click", function () {
+  $("#results").html("Loading...");
+
+  let promise = $.ajax({
+    type: "GET",
+    url: "https://api.github.com/orgs/emberjs/repos",
+  });
+
+  promise.then((repos) => {
+    let fragment = document.createDocumentFragment();
+
+    repos.forEach((repo) => {
+      let div = document.createElement("div");
+      let h3 = document.createElement("h3");
+      let p = document.createElement("p");
+
+      h3.textContent = repo.name;
+      p.textContent = repo.description;
+
+      div.append(h3);
+      div.append(p);
+      fragment.append(div);
+    });
+
+    $("#results").html(fragment);
+  });
+});
